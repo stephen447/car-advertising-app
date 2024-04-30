0ce0c0c964fad06c76c27064495d49b680f69257
stephen447 <byrnes67@tcd.ie>
2024-05-01 00:56:00 +0100

    memory efficiency

--- frontend/src/components/createAdvertForm/createAdvertForm.jsx
+++ frontend/src/components/createAdvertForm/createAdvertForm.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import "./sellYourCar.css";
-import image from "../media/images/testImage.jpg";
+// import image from "../media/images/testImage.jpg";
 
 export default function CreateAdvertForm() {
   return (
     <div className="sellYourCar">
-      <img className="sellYourCarImage" src={image}></img>
+      {/* //<img className="sellYourCarImage" src={image}></img> */}
       <div className="sellYourCarText"></div>
     </div>
   );
--- frontend/src/components/featuredAdvert/featuredAdvert.jsx
+++ frontend/src/components/featuredAdvert/featuredAdvert.jsx
@@ -6,7 +6,6 @@ export default function FeaturedAdvert(props) {
     <div className="individualFeaturedAdvert">
       <h2> {props.manufacturer}</h2>
       <h3> {props.model}</h3>
-      <img className="individualFeaturedAdvert_image" src={props.src}></img>
       <h4>{props.year}</h4>
       <p>{props.mileage} km</p>
     </div>
